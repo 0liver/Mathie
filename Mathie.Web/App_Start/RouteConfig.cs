f4a80fc63377a0f13f1de5a5a9a57229f2a4d8c7
Oliver <oliver.friedrich@gmail.com>
2012-08-28 17:39:48 +0200

    Mathie.Web: first shot at form

--- Mathie.Web/App_Start/RouteConfig.cs
+++ Mathie.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Mathie
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "Equation", action = "Index", id = UrlParameter.Optional }
 			);
 		}
 	}
---
+++ Mathie.Web/Controllers/EquationController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using Mathie.Models;
+
+namespace Mathie.Controllers {
+	public class EquationController : Controller {
+		[HttpGet]
+		public ActionResult Index() {
+			var randomNumber = GetRandomNumber();
+			return View((object) randomNumber);
+		}
+
+		[HttpPost]
+		public ActionResult Index(string equation) {
+			var solver = new EquationSolver();
+			if (solver.IsInCorrectFormat(equation)) {
+				if (solver.Solve(equation)) {
+					return RedirectToAction("Index");
+				}
+				ViewData["Message"] = "Format error - try again!";
+				return View();
+			}
+			ViewData["Message"] = "Sides not equal - try again!";
+			return View();
+		}
+
+		private string GetRandomNumber() {
+			return 342652.ToString();
+		}
+	}
+}
